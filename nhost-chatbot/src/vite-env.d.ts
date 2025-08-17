f94c0d186a03e628e8396e4d8d103e8427249059
vbsubspace <vedashribandewar@gmail.com>
2025-08-18 03:17:15 +0530

    Update vite-env.d.ts

--- nhost-chatbot/src/vite-env.d.ts
+++ nhost-chatbot/src/vite-env.d.ts
@@ -1,8 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_NHOST_SUBDOMAIN?: string
-  readonly VITE_NHOST_REGION?: string
   readonly VITE_NHOST_BACKEND_URL?: string
 }
 
